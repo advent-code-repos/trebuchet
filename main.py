73e210a7b3754182feb0bee023241e2e8954d3b3
Luca Maggio <lucamaggio1992@gmail.com>
2024-11-28 20:26:01 +0100

    build(day1): lint whole project

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from services.calibration_services import CalibrationService
 from config.logger import logger
+from services.calibration_services import CalibrationService
+
 
 def main():
-  logger.info("Advent Calendar 2023 starts")
+    logger.info("Advent Calendar 2023 starts")
 
-  document = "calibration-document"
-  calibration_service = CalibrationService(document) 
-  logger.info("CalibrationService initiated")
+    document = "calibration-document"
+    calibration_service = CalibrationService(document)
+    logger.info("CalibrationService initiated")
 
-  calibration_value = calibration_service.recalibrate()
-  logger.info(f"Recalibration is terminate with this calibration value: {calibration_value}")
+    calibration_value = calibration_service.recalibrate()
+    logger.info(
+        "Recalibration is terminate with this "
+        f"calibration value: {calibration_value}"
+    )
+
 
 if __name__ == "__main__":
     main()
